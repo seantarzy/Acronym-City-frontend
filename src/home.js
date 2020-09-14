40719ed506ad8ea5c7496cab72108b3715591e4d
Sean Tarzy <seantarzy@gmail.com>
2020-09-14 17:03:48 -0400

    spring cleaning

--- src/home.js
+++ src/home.js
@@ -10,11 +10,7 @@ import { fetchQuery } from "./services/utils";
 import Lyrics from './lyrics.js'
 class Home extends React.Component {
   state = {
-    selectedOption: null,
-    searchList: ["red", "blue", "yellow"],
     searchPhrase: "",
-    lyrics: "",
-    moneyLyrics: ""
   };
 
   handleChange = (e) => {
@@ -23,36 +19,18 @@ class Home extends React.Component {
     console.log(this.state);
   };
 
-  boldLyrics = (songInfo) => {
-    const lyrics = document.getElementsByClassName('lyrics')
-
-    songInfo.lyrics.forEach((lyric, index) => {
-
-      if (index > songInfo.index_at - 1 && index < songInfo.length + songInfo.index_at){
-        const b = document.createElement('b')
-        b.innerText = lyric
-        lyrics.appendChild(b)
-      } else if (index <= songInfo.index_at - 1) {
-        
-      } else if (index >= songInfo.length + songInfo.index_at){
-
-      }
-    })
-    // return lyrics
-  }
-
   handleLyrics = (lyrics) =>{
     let lyricsDiv = document.getElementById('lyrics')
     if(lyrics){
       let p1 = document.createElement('p')
       p1.innerText = lyrics[0]
-    lyricsDiv.appendChild(p1)
-    let boldStuff = document.createElement("b");
-    boldStuff.innerText = lyrics[1]
-       lyricsDiv.appendChild(boldStuff);
-       let p2 = document.createElement("p");
-       p2.innerText = lyrics[2]
-       lyricsDiv.appendChild(p2)
+      lyricsDiv.appendChild(p1)
+      let boldStuff = document.createElement("b");
+      boldStuff.innerText = lyrics[1]
+      lyricsDiv.appendChild(boldStuff);
+      let p2 = document.createElement("p");
+      p2.innerText = lyrics[2]
+      lyricsDiv.appendChild(p2)
     }
     else{
       let error = document.createElement('p')
@@ -64,24 +42,11 @@ class Home extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    // let initials = this.getTheInitials(this.state.searchPhrase)
     fetchQuery(this.state.searchPhrase).then((r) => {
       console.log(r.result)
       this.handleLyrics(r.result.lyrics_array)
-    //  let boldedLyrics = this.boldLyrics(r.result) 
-      // this.setState({ moneyLyrics: boldedLyrics});
     });
-    // console.log(initials)
   };
-
-  // getTheInitials(stringPhrase){
-  //     let initialsArray = []
-  //     let stringArray = stringPhrase.split(' ')
-  //     for(let i = 0; i<stringArray.length; i++){
-  //         initialsArray.push(stringArray[i][0].toLowerCase())
-  //     }
-  //     return initialsArray
-  // }
 
   render() {
     return (
@@ -102,12 +67,10 @@ class Home extends React.Component {
               Search
             </Button>
           </Form>
-            {/* <Lyrics lyrics = {this.state.moneyLyrics}/> */}
             <div class='lyrics' id="lyrics">
               
             </div>
         </Navbar>
-        <p id="result"></p>
         <br />
       </>
     );
